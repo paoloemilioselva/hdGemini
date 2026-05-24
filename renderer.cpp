e3286edf2a50a16a890a03a063e48e20e7471f6e
Paolo Emilio Selva <paoloemilioselva@gmail.com>
2026-05-24 21:38:06 +1200

    Restrict global ocean bounds when repeat is disabled

--- renderer.cpp
+++ renderer.cpp
@@ -527,6 +527,10 @@ HdGeminiRenderer::_PrepareScene(HdRenderThread *renderThread, HdGeminiRenderDele
             GfMatrix4f viewProj(GfMatrix4f(_viewMatrix) * GfMatrix4f(_projMatrix));
             GfVec3f camPos(GfVec3f(_viewMatrix.GetInverse().ExtractTranslation()));
             GfRange3f worldBounds(-GfVec3f(100000.0f), GfVec3f(100000.0f));
+            if (!_oceanParams.repeat) {
+                float halfSize = _oceanParams.size * 0.5f;
+                worldBounds = GfRange3f(GfVec3f(-halfSize, -10000.0f, -halfSize), GfVec3f(halfSize, 10000.0f, halfSize));
+            }
             
             _globalOceanBasePoints.clear();
             _globalOceanIndices.clear();
